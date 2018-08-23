00783a69a511e5027cadbba8776d333ab93330d0
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-08-23 20:20:00 +1200

    updated photolist screen.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,7 +5,10 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
+
 import SettingsScreen from '../screens/SettingsScreen';
+import PhotoListScreen from '../screens/PhotoListScreen';
+
 import DefaultScreen from '../screens/DefaultScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import PhotoScreen from '../screens/PhotoScreen';
@@ -33,7 +36,7 @@ HomeStack.navigationOptions = {
 };
 
 const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+  Links: PhotoListScreen,
 });
 
 LinksStack.navigationOptions = {
---
+++ screens/PhotoListScreen.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text, Button,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { WebBrowser } from 'expo';
+import { MonoText } from '../components/StyledText';
+import { styles } from '../shared/css/style';
+import { Icon, List, ListItem } from 'react-native-elements';
+
+export default class PhotoListScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  list = [
+    {
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President', 
+      icon: 'av-timer'
+    },
+    {
+      name: 'Chris Jackson',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman',
+      icon: 'flight-takeoff'
+
+    },
+  ];  
+  
+  render() {
+    
+    return (
+      <View style={styles.container}>
+
+      <List containerStyle={{marginBottom: 20}}>
+        {
+          this.list.map((l, i) => (
+
+            <ListItem
+              roundAvatar
+              avatar={{uri:l.avatar_url}}
+              key={i}
+              title={l.name}
+              leftIcon={{name: l.icon}}
+            />
+          ))
+
+        }
+</List>
+     
+      
+      </View>
+    );
+  }
+  
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+        Learn more
+        </Text>
+      );
+      
+      return (
+        <Text style={styles.developmentModeText}>
+        Development mode is enabled, your app will be slower but you can use useful development
+        tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+        You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+  
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+  
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
+}
+
