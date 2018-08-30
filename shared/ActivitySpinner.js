443a8d1d82029c652187a84115a1e78d5b72d2d2
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-08-30 19:12:06 +1200

    fix spinner issue.

--- shared/ActivitySpinner.js
+++ shared/ActivitySpinner.js
@@ -8,10 +8,10 @@ import {
 
 export const ActivitySpinner = ({isBusy}) => { 
     return (
-        <View>
-           <ActivityIndicator style={{ position : 'absolute', left: 0, 
+       
+           <ActivityIndicator style={{position : 'absolute', left: 0, 
             right: 0, top: 0, bottom: 0, alignItems: 'center',justifyContent: 'center'}}
             animating={isBusy} color = '#0a1640' size = "large" /> 
-        </View>
+      
     );
   };  
